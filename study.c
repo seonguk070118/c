3ca714d45a0010d25662791d1a3fb09cf0c5d14f
seonguk070118 <seonguk070118@gmail.com>
2023-04-01 23:33:22 +0900

    a*123456789

--- study.c
+++ study.c
@@ -1,29 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<limits.h>
 int main()
 {
-    int j[2],y,d,m;
-    scanf("%d-%d",&j[0],&j[1]);
-    y=j[0]/10000;
-    m=(j[0]-y*10000)/100;
-    d=j[0]-y*10000-m*100;
-    if(j[1]/1000000==1)
-    {
-            printf("19%.2d/%.2d/%.2d M",y,m,d);
-    }
-    if(j[1]/1000000==2)
-    {
-
-            printf("19%.2d/%.2d/%.2d F",y,m,d);
-    }
-    if(j[1]/1000000==3)
-    {
-
-            printf("20%.2d/%.2d/%.2d M",y,m,d);
-    }
-    if(j[1]/1000000==4)
-    {
-            printf("20%.2d/%.2d/%.2d F",y,m,d);
-    }
+    int a;
+    scanf("%d",&a);
+    printf("%lld", a*123456789LL);
     return 0;
 }
 
