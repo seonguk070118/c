5469802f571014d5365a917a8253d7207d2a664e
seonguk070118 <seonguk070118@gmail.com>
2023-04-06 14:52:17 +0900

    Oven clock

--- study.c
+++ study.c
@@ -3,30 +3,28 @@
 #include<limits.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    if(a<=b){
-    	if(a<=c){
-    		if(b<=c) printf("%d",b);
-    		else printf("%d",c);
+    int h,m,t,lm;
+    scanf("%d %d\n%d",&h,&m,&t);
+    if(m+t>60)
+    {
+    	lm=(m+t)/60;
+    	m=(m+t)%60;
+    	if(h+lm>23)
+    	{
+    		h=h+lm-24;
+    		printf("%d %d",h,m);
 		}
-		else printf("%d",a);
-	}
-	else if(b<=a){
-		if(b<=c){
-			if(a<=c) printf("%d",a);
-			else printf("%d",c);
+		else
+		{
+			h=h+lm;
+			printf("%d %d",h,m);
 		}
-		else printf("%d",b);
 	}
-	else if(c<=a){
-		if(c<=b){
-			if(b<=a) printf("%d",b);
-			else printf("%d",a);
-		}
-		else printf("%d",c);
+	else
+	{
+		m=m+t;
+		printf("%d %d",h,m);
 	}
-    else printf("%d",a);
 	return 0;
 }
 
