12e84af2d8cd9f47c35d45f4e4ac20a614f32320
seonguk070118 <seonguk070118@gmail.com>
2023-04-13 15:34:14 +0900

    4051_codeup

--- study.c
+++ study.c
@@ -5,12 +5,19 @@
 
 int main()
 {
-    int y,m,d,sum;
-    scanf("%d %d %d",&y,&m,&d);
-    sum = y+m+d;
-    sum = sum/100;
-    if(sum%2==0) printf("대박");
-    else printf("그럭저럭");
+    float s,l,t,i,at=0,p=0;
+    for(i=0;i<5;i++)
+    {
+    	scanf("%f %f",&s,&l);
+    	t = l-s;
+    	t = t-1;
+    	if(t>4) t=4;
+    	at+=t;
+    	if(t>0) p+=(t*10000);
+	}
+	if(at>=15) p=p-(p*0.05);
+	else if(at<=5) p=p+(p*0.05);
+	printf("%.0f",p);
     return 0;
 }
 
