1504e20bde38b78e3e3e8da31acfbe06782434b7
seonguk070118 <seonguk070118@gmail.com>
2023-04-15 23:49:42 +0900

    1168_codeup

--- study.c
+++ study.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<limits.h>
-#include <stdlib.h>
 
 int main()
 {
-    float s,l,t,i,at=0,p=0;
-    for(i=0;i<5;i++)
-    {
-    	scanf("%f %f",&s,&l);
-    	t = l-s;
-    	t = t-1;
-    	if(t>4) t=4;
-    	at+=t;
-    	if(t>0) p+=(t*10000);
-	}
-	if(at>=15) p=p-(p*0.05);
-	else if(at<=5) p=p+(p*0.05);
-	printf("%.0f",p);
+    int y,s;
+    scanf("%d %d",&y,&s);
+    if(s==1 || s==2) printf("%d",100-(y/10000)+13);
+    else printf("%d",13-(y/10000));
     return 0;
 }
 
