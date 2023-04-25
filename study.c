e3c6f037b76fd393698ce3b60208ece0fed79f6f
seonguk070118 <seonguk070118@gmail.com>
2023-04-25 17:39:24 +0900

    1355_codeup

--- study.c
+++ study.c
@@ -1,76 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<limits.h>
-#include<time.h>
-
-int random_(void);
-void input_value(int random_2);
-void output_value(void);
-
-int random=0,i,j;
-char a[3][3]={{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
 
 int main()
 {
-	input_value(5);
-	output_value();
-	
-    return 0;
-}
-
-int random_(void)
-{
-	srand(time(NULL));
-	random = rand()%6+1;
-	return random;
-}
-
-void input_value(int random_2)
-{
-	if(random_2==1){
-		a[1][1]='*';
-	}
-	else if(random_2==2){
-		a[0][2]='*';
-		a[2][0]='*';
-	}
-	else if(random_2==3){
-		a[0][2]='*';
-		a[2][0]='*';
-		a[1][1]='*';
-	}
-	else if(random_2==4){
-		a[0][0]='*';
-		a[0][2]='*';
-		a[2][0]='*';
-		a[2][2]='*';
-	}
-	else if(random_2==5){
-		a[0][0]='*';
-		a[0][2]='*';
-		a[2][0]='*';
-		a[2][2]='*';
-		a[1][1]='*';
-	}
-	else if(random_2==6){
-		a[0][0]='*';
-		a[0][2]='*';
-		a[1][0]='*';
-		a[1][2]='*';
-		a[2][0]='*';
-		a[2][2]='*';
-	}
-}
-
-void output_value(void)
-{
-	for(i=0;i<3;i++)
+    int s,i,j,k,l=0;
+    scanf("%d",&s);
+    for(i=s;i>=1;i--)
 	{
-		for(j=0;j<3;j++)
+		for(k=0;k<l;k++)
 		{
-			printf("%c",a[i][j]);
+			printf(" ");
+		}
+		l++;
+    	for(j=i;j>=1;j--)
+		{
+    		printf("*");
 		}
 		printf("\n");
 	}
+    return 0;
 }
 
