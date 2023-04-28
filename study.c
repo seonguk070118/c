d3bcd4f15a3bcc589c95ef8d7bc5cbaf03db7f47
seonguk070118 <seonguk070118@gmail.com>
2023-04-28 21:28:52 +0900

    1231_codeup

--- study.c
+++ study.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<limits.h>
+#include <stdlib.h>
 
 int main()
 {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    printf("%d%.2d%.3d",a,b,c);
-	return 0;
+    int n,m;
+    char b;
+    scanf("%d%c%d",&n,&b,&m);
+    switch(b){
+    	case '+':printf("%d",n+m); break;
+    	case '-':printf("%d",n-m); break;
+    	case '*':printf("%d",n*m); break;
+    	case '/':printf("%.2f",(float)n/m); break;
+    	
+    	
+	}
+    return 0;
 }
-
