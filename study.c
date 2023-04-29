5ebe766bb37cf42507cb38bdffce92fcf2d613e0
seonguk070118 <seonguk070118@gmail.com>
2023-04-29 23:33:35 +0900

    1172_codeup

--- study.c
+++ study.c
@@ -5,16 +5,21 @@
 
 int main()
 {
-    int n,m;
-    char b;
-    scanf("%d%c%d",&n,&b,&m);
-    switch(b){
-    	case '+':printf("%d",n+m); break;
-    	case '-':printf("%d",n-m); break;
-    	case '*':printf("%d",n*m); break;
-    	case '/':printf("%.2f",(float)n/m); break;
-    	
-    	
+    int i,a[3],max=0,min=100,front=78958;
+    scanf("%d %d %d",&a[0],&a[1],&a[2]);
+    for(i=0;i<3;i++)
+    {
+    	if(a[i]>=max) max=a[i];
+    	if(a[i]<=min) min=a[i];
 	}
+	for(i=0;i<3;i++)
+    {
+		if(a[i]!=max && a[i]!=min) front=a[i];
+		if(a[i]==max && a[i]==min) front=a[i]; 
+	}
+	if(a[0]==a[1]) front=a[0];
+	if(a[0]==a[2]) front=a[0];
+	if(a[1]==a[2]) front=a[1];
+    printf("%d %d %d",min,front,max);
     return 0;
 }
