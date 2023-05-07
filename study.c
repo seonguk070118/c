0d79b200aed0502084caa658e080e23668f4b3d4
seonguk070118 <seonguk070118@gmail.com>
2023-05-07 22:26:00 +0900

    1204_codeup

--- study.c
+++ study.c
@@ -4,9 +4,34 @@
 
 int main()
 {
-	double a,b,c;
-	scanf("%lf %lf",&a,&b);
-	c=a/b;
-	printf("%.10lf",c);
+	int a;
+	scanf("%d",&a);
+	if(a%10==1){
+		if(a==11){
+			printf("%dth",a);
+		}
+		else{
+			printf("%dst",a);
+		}
+	}
+	else if(a%10==2){
+		if(a==12){
+			printf("%dth",a);
+		}
+		else{
+			printf("%dnd",a);
+		}
+	}
+	else if(a%10==3){
+		if(a==13){
+			printf("%dth",a);
+		}
+		else{
+			printf("%drd",a);
+		}
+	}
+	else{
+		printf("%dth",a);
+	}
 	return 0;
 }
