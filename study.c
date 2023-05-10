af8975c59b4cce0025ed6a1e50f2b18650d8b9be
seonguk070118 <seonguk070118@gmail.com>
2023-05-10 12:13:36 +0900

    5/10_function.study

--- study.c
+++ study.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<limits.h>
-
+int f(int n)
+	{
+		if(n%400==0)
+			return 1;
+		else if(n%4==0&&n%100!=0)
+			return 1;
+		else
+			return 0; 
+	}
+	
 int main()
 {
-	int a;
-	scanf("%d",&a);
-	if(a%10==1){
-		if(a==11){
-			printf("%dth",a);
-		}
-		else{
-			printf("%dst",a);
-		}
-	}
-	else if(a%10==2){
-		if(a==12){
-			printf("%dth",a);
-		}
-		else{
-			printf("%dnd",a);
-		}
-	}
-	else if(a%10==3){
-		if(a==13){
-			printf("%dth",a);
-		}
-		else{
-			printf("%drd",a);
-		}
+	int n;
+	scanf("%d",&n);
+	if(f(n)==1)
+	{
+		printf("À±³â");
 	}
 	else{
-		printf("%dth",a);
+		printf("Æò³â");
 	}
 	return 0;
 }
