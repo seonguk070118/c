7910bcae117cc5530bfb283619186c9fa4f5a397
seonguk070118 <seonguk070118@gmail.com>
2023-05-11 14:33:51 +0900

    1403_codeup

--- study.c
+++ study.c
@@ -10,9 +10,13 @@ int main()
 	{
 		scanf("%d",&a[i]);
 	}
-	for(i=b;i>=1;i--)
+	for(i=1;i<=b;i++)
 	{
-		printf("%d ",a[i]);
+		printf("%d\n",a[i]);
+	}
+	for(i=1;i<=b;i++)
+	{
+		printf("%d\n",a[i]);
 	}
 	return 0;
 }
