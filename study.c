425e3a4869292d543b1164aeb272c87cd72a60b9
seonguk070118 <seonguk070118@gmail.com>
2023-05-11 15:08:52 +0900

    1405_codeup

--- study.c
+++ study.c
@@ -4,19 +4,24 @@
 
 int main()
 {
-	int a[1001]={},b,i;
+	int a[1001]={},b,i,j,k=0,x=0;
 	scanf("%d",&b);
-	for(i=1;i<=b;i++)
+	for(i=0;i<b;i++)
 	{
 		scanf("%d",&a[i]);
 	}
-	for(i=1;i<=b;i++)
+	for(i=0;i<b;i++)
 	{
-		printf("%d\n",a[i]);
-	}
-	for(i=1;i<=b;i++)
-	{
-		printf("%d\n",a[i]);
+		for(j=0;j<b;j++)
+		{
+			printf("%d ",a[k+x]);
+			k++;
+			if(k+x>=b)
+				k=k-b;
+		}
+		x++;
+		k=0;
+		printf("\n");
 	}
 	return 0;
 }
