c5757975963b03d74265955b4e36e7fb1fd014b0
seonguk070118 <seonguk070118@gmail.com>
2023-05-13 22:37:34 +0900

    proprammers_LV0

--- study.c
+++ study.c
@@ -5,40 +5,12 @@
 
 int main(){
  
-    int a,b,mx=0,i,sum;
-    scanf("%d %d",&a,&b);
-    sum=a+b;
-   	if(sum>mx)
-   		mx=sum;
-   	sum=a-b;
-   	if(sum>mx)
-   		mx=sum;
-   	sum=a*b;
-   	if(sum>mx)
-   		mx=sum;
-   	sum=a/b;
-   	if(sum>mx)
-   		mx=sum;
-   	sum=b-a;
-   	if(sum>mx)
-   		mx=sum;
-   	sum=b/a;
-   	if(sum>mx)
-   		mx=sum;
-   	sum=a;
-   	for(i=1;i<b;i++)
-   	{
-   		sum=sum*a;
+    char str[11]={};
+    int n,i;
+    scanf("%s %d",str,&n);
+    for(i=0;i<n;i++)
+    {
+    	printf("%s",str);
 	}
-	if(sum>mx)
-   		mx=sum;
-	sum=b;
-   	for(i=1;i<a;i++)
-   	{
-   		sum=sum*b;
-	}
-	if(sum>mx)
-   		mx=sum;
-	printf("%.6f",(float)mx);
     return 0;
 }
