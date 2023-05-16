0ffe128c56b4a07285c8845ab2ca8cdf0bc19092
seonguk070118 <seonguk070118@gmail.com>
2023-05-16 15:11:02 +0900

    4847_codeup

--- study.c
+++ study.c
@@ -5,9 +5,18 @@
 
 int main(){
  
-    char str[1000001]={};
-    scanf("%s",str);
-   	printf("%s",str);
-
+    int a,b,bm=1;
+    int m,i,s,sf=0;
+    scanf("%d",&m);
+    scanf("%d %d %d",&a,&b,&s);
+    sf+=s;
+    bm=b;
+    for(i=0;i<m-1;i++)
+    {
+    	scanf("%d %d %d",&a,&b,&s);
+    	sf+=s;
+    	bm=b*(bm/a);
+	}
+	printf("%d %d",sf%2,bm);
     return 0;
 }
