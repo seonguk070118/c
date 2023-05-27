9214191d3b02b2811911f514946b8d884f9fe5fe
seonguk070118 <seonguk070118@gmail.com>
2023-05-27 23:43:47 +0900

    1216_codeup

--- study.c
+++ study.c
@@ -6,30 +6,20 @@
 
 int main()
 {
-	int year,month;
-	scanf("%d %d",&year,&month);
-	if(month==2)
+	int a,b,c,s;
+	scanf("%d %d %d",&a,&b,&c);
+	s=a+c;
+	if(b>s)
 	{
-		if(year%400==0)
-		{
-			printf("29");
-		}
-		else if(year%4==0 && year%100!=0)
-		{
-			printf("29");
-		}
-		else
-		{
-			printf("28");
-		}
+		printf("advertise");
 	}
-	else if(month==4 || month==6 || month==9 || month==11)
+	else if(b<s)
 	{
-		printf("30");
+		printf("do not advertise");
 	}
 	else
 	{
-		printf("31");
+		printf("does not matter");
 	}
 	return 0;
 }
