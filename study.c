d2bf69d3379f40146ca04a99c34a18a7b87357a1
seonguk070118 <seonguk070118@gmail.com>
2023-05-29 22:39:53 +0900

    1218_codeup

--- study.c
+++ study.c
@@ -5,12 +5,67 @@
 #include <stdbool.h>
 int main()
 {
-	int a[10]={},b,i;
-	for(i=0;i<10;i++)
-	{
-		scanf("%d",&a[i]);
+	int a,b,c,max=0;
+	scanf("%d %d %d",&a,&b,&c);
+	if(max<a) max=a;
+	if(max<b) max=b;
+	if(max<c) max=c;
+	if(max==a && a<b+c){
+		if(a==b && b==c)
+		{
+			printf("Á¤»ï°¢Çü");
+		}
+		else if(a==b || b==c || c==a)
+		{
+			printf("ÀÌµîº¯»ï°¢Çü");
+		}
+		else if((a*a)==(b*b)+(c*c))
+		{
+			printf("Á÷°¢»ï°¢Çü");
+		}
+		else
+		{
+			printf("»ï°¢Çü");
+		}
 	}
-	scanf("%d",&b);
-	printf("%d",a[b-1]);
+	else if(max==b && b<a+c){
+		if(a==b && b==c)
+		{
+			printf("Á¤»ï°¢Çü");
+		}
+		else if(a==b || b==c || c==a)
+		{
+			printf("ÀÌµîº¯»ï°¢Çü");
+		}
+		else if((b*b)==(a*a)+(c*c))
+		{
+			printf("Á÷°¢»ï°¢Çü");
+		}
+		else
+		{
+			printf("»ï°¢Çü");
+		}
+	}
+	else if(max==c && c<b+a){
+		if(a==b && b==c)
+		{
+			printf("Á¤»ï°¢Çü");
+		}
+		else if(a==b || b==c || c==a)
+		{
+			printf("ÀÌµîº¯»ï°¢Çü");
+		}
+		else if((c*c)==(b*b)+(a*a))
+		{
+			printf("Á÷°¢»ï°¢Çü");
+		}
+		else
+		{
+			printf("»ï°¢Çü");
+		}
+	}
+	else{
+		printf("»ï°¢Çü¾Æ´Ô");
+	}
 	return 0;
 }
