881ba260a9e90c22b02ee85e0545de1856b2c657
seonguk070118 <seonguk070118@gmail.com>
2023-06-02 23:03:06 +0900

    1229_codeup

--- study.c
+++ study.c
@@ -5,15 +5,23 @@
 #include <stdbool.h>
 int main()
 {
-	int a,b,c;
-	scanf("%d %d %d",&a,&b,&c);
-	if(a<=170)
-		printf("CRASH %d",a);
-	else if(b<=170)
-		printf("CRASH %d",b);
-	else if(c<=170)
-		printf("CRASH %d",c);
-	else
-		printf("PASS");
+	double a,b,p,body;
+	scanf("%lf %lf",&a,&b);
+	if(a<150){
+		p=(a-100);
+	}
+	else if(a<160&&a>=150){
+		p=(a-150)/2+50;
+	}
+	else if(a>=160){
+		p=(a-100)*0.9;
+	}
+	body=(b-p)*100/p;
+	if(body<10)
+		printf("정상");
+	else if(body<=20&&body>10)
+		printf("과체중");
+	else if(body>20)
+		printf("비만");
     return 0;
 }
