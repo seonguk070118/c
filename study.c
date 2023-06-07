a86a0b2b3a220053439980c9dd1ddf2638dc710e
seonguk070118 <seonguk070118@gmail.com>
2023-06-07 11:23:03 +0900

    Fibonacci_sequence_easy

--- study.c
+++ study.c
@@ -3,23 +3,17 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
-int main()
+int f(int x){
+	if(x==0)
+		return 0;
+	if(x==1)
+		return 1;
+	return (f(x-1)+f(x-2));
+}
+int main(void)
 {
-	int i,n,m;
-	scanf("%d %d",&n,&m);
-	if(n>=m)
-	{
-		for(i=m;i<=n;i++)
-		{
-			printf("%d ",i);
-		}
-	}
-	else
-	{
-		for(i=n;i<=m;i++)
-		{
-			printf("%d ",i);
-		}
-	}
+	int n;
+	scanf("%d",&n);
+	printf("%d",f(n));
     return 0;
 }
