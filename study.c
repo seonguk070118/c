3fbe2d36a032de8e17cfd3f410e606c234ff4b62
seonguk070118 <seonguk070118@gmail.com>
2023-06-08 22:32:23 +0900

    1254_codeup

--- study.c
+++ study.c
@@ -3,17 +3,24 @@
 #include<limits.h>
 #include<string.h>
 #include <stdbool.h>
-int f(int x){
-	if(x==0)
-		return 0;
-	if(x==1)
-		return 1;
-	return (f(x-1)+f(x-2));
-}
-int main(void)
+int main()
 {
-	int n;
-	scanf("%d",&n);
-	printf("%d",f(n));
+	int i;
+	char n,m;
+	scanf("%c %c",&n,&m);
+	if(n>=m)
+	{
+		for(i=m;i<=n;i++)
+		{
+			printf("%c ",i);
+		}
+	}
+	else
+	{
+		for(i=n;i<=m;i++)
+		{
+			printf("%c ",i);
+		}
+	}
     return 0;
 }
