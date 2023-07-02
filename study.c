984027cd62dc8076bdd278739030331ef7699528
seonguk070118 <seonguk070118@gmail.com>
2023-07-02 23:05:58 +0900

    1275_codeup

--- study.c
+++ study.c
@@ -6,17 +6,18 @@
 
 int main()
 {
-	int a,i;
-	scanf("%d",&a);
-	for(i=2;i<a;i++)
-	{
-		if(a%i==0)
-		{
-			printf("not prime");
-			return 0;
-		}
+	int a,b,i,c;
+	scanf("%d %d",&a,&b);
+	c=a;
+	if(b==0){
+		printf("1");
+		return 0;
 	}
-	printf("prime");
+	for(i=1;i<b;i++)
+	{
+		a=a*c;
+	}
+	printf("%d",a);
 	return 0;
 }
 
