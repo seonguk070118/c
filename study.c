a23817bb021a64da3a69607bacbe2d1b70d14b3d
seonguk070118 <seonguk070118@gmail.com>
2023-07-09 23:25:07 +0900

    1281_codeup

--- study.c
+++ study.c
@@ -8,7 +8,17 @@ int main()
 {
 	int a,b,i,sum=0;
 	scanf("%d %d",&a,&b);
-	for(i=a;i<=b;i++)
+	if(a%2==0)
+	{
+		sum-=a;
+		printf("-%d",a);
+	}
+	else if(a%2==1)
+	{
+		sum+=a;
+		printf("%d",a);
+	}
+	for(i=a+1;i<=b;i++)
 	{
 		if(i%2==0)
 		{
