9685c6f48d6a66df768ca280b8590928498e2b2d
seonguk070118 <seonguk070118@gmail.com>
2023-07-11 22:42:58 +0900

    1001_baekjoon

--- study.c
+++ study.c
@@ -6,32 +6,9 @@
 
 int main()
 {
-	int a,b,i,sum=0;
+	int a,b;
 	scanf("%d %d",&a,&b);
-	if(a%2==0)
-	{
-		sum-=a;
-		printf("-%d",a);
-	}
-	else if(a%2==1)
-	{
-		sum+=a;
-		printf("%d",a);
-	}
-	for(i=a+1;i<=b;i++)
-	{
-		if(i%2==0)
-		{
-			sum-=i;
-			printf("-%d",i);
-		}
-		else if(i%2==1)
-		{
-			sum+=i;
-			printf("+%d",i);
-		}
-	}
-	printf("=%d",sum);
+	printf("%d",a-b);
 	return 0;
 }
 
